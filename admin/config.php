51ae42d54793287ad0bab1478ceee1157d23d25c
ddonthegiutar <gwynnbleidd@gmail.com>
2017-05-12 10:57:38 +0300

    start

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://liquid2.dev/admin/');
+define('HTTP_CATALOG', 'http://liquid2.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://liquid2.dev/admin/');
+define('HTTPS_CATALOG', 'http://liquid2.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/liquid2/admin/');
+define('DIR_SYSTEM', '/var/www/liquid2/system/');
+define('DIR_IMAGE', '/var/www/liquid2/image/');
+define('DIR_LANGUAGE', '/var/www/liquid2/admin/language/');
+define('DIR_TEMPLATE', '/var/www/liquid2/admin/view/template/');
+define('DIR_CONFIG', '/var/www/liquid2/system/config/');
+define('DIR_CACHE', '/var/www/liquid2/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/liquid2/system/storage/download/');
+define('DIR_LOGS', '/var/www/liquid2/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/liquid2/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/liquid2/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/liquid2/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'qwe012');
+define('DB_DATABASE', 'liquid2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'll_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://liquid2.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://liquid2.dev/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/liquid2/catalog/');
+define('DIR_SYSTEM', '/var/www/liquid2/system/');
+define('DIR_IMAGE', '/var/www/liquid2/image/');
+define('DIR_LANGUAGE', '/var/www/liquid2/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/liquid2/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/liquid2/system/config/');
+define('DIR_CACHE', '/var/www/liquid2/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/liquid2/system/storage/download/');
+define('DIR_LOGS', '/var/www/liquid2/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/liquid2/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/liquid2/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'qwe012');
+define('DB_DATABASE', 'liquid2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'll_');
